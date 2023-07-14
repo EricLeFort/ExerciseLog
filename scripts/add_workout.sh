8fbfe63b256cb50fc0218d7ff602fba113a5a113
Eric Le Fort <ericlefort@live.com>
2023-07-14 13:35:58 -0700

    Extended add-workout script to use flags to enable amend and dryrun modes

--- scripts/add_workout.sh
+++ scripts/add_workout.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
+DATE_FORMAT=[0-9]{2}-[A-Z]{3}-[0-9]{4}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ $# -eq 0 ] || ! [[ $1 =~ [0-9]{2}-[A-Z]{3}-[0-9]{4} ]]
+# Read in args
+dryrun=false
+amendCommit=false
+date=false
+
+for arg in "$@"
+do
+    case "$arg" in
+    "-d"|"--dryrun")    dryrun=true
+                        ;;
+    "-a"|"--amend")     amendCommit=true
+                        ;;
+    *)                  if [[ ${arg} =~ \-\-.* ]]
+                        then
+                            echo "Invalid flag ${1}. Allowed arguments are (-d|--dryrun; -a|--amend)"
+                            exit 1
+                        elif [[ ${arg} =~ ${DATE_FORMAT} ]]
+                        then
+                            date=${arg}
+                        else
+                            echo "A date should be passed in as the first argument in the form DD-MMM-YYYY e.g. 01-JAN-2023"
+                            exit 1
+                        fi
+                        ;;
+    esac
+    shift
+done
+
+if [ ${dryrun} = true ] && [ ${amendCommit} = true ]
 then
-    echo "A date should be passed in as the first argument in the form DD-MMM-YYYY e.g. 01-JAN-2023"
+    echo "Cannot set both dryrun and amend"
     exit 1
 fi
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Update visuals
 export PYTHONPATH="${SCRIPT_DIR}/../src/"
 python3 "${SCRIPT_DIR}/../src/exercise_log/update_visuals.py"
 
 # Add the new commit
-git add data img
-git commit -m "Added $1 workout"
+if [ ${dryrun} = false ]
+then
+    git add data img
+    if [ ${amendCommit} = true ]
+    then
+        git commit --amend --no-edit
+    else
+        git commit -m "Added $1 workout"
+    fi
+fi
