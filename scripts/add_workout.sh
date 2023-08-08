66bdf542bb9a62e135ee8175424d71b4b8bf2960
Eric Le Fort <ericlefort@live.com>
2023-08-08 11:31:01 -0700

    Modified add_workout.sh to exit on error

--- scripts/add_workout.sh
+++ scripts/add_workout.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 DATE_FORMAT=[0-9]{2}-[A-Z]{3}-[0-9]{4}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -10,22 +12,25 @@ date=false
 for arg in "$@"
 do
     case "$arg" in
-    "-d"|"--dryrun")    dryrun=true
-                        ;;
-    "-a"|"--amend")     amendCommit=true
-                        ;;
-    *)                  if [[ ${arg} =~ \-\-.* ]]
-                        then
-                            echo "Invalid flag ${1}. Allowed arguments are (-d|--dryrun; -a|--amend)"
-                            exit 1
-                        elif [[ ${arg} =~ ${DATE_FORMAT} ]]
-                        then
-                            date=${arg}
-                        else
-                            echo "A date should be passed in as the first argument in the form DD-MMM-YYYY e.g. 01-JAN-2023"
-                            exit 1
-                        fi
-                        ;;
+    "-d"|"--dryrun")
+        dryrun=true
+        ;;
+    "-a"|"--amend")
+        amendCommit=true
+        ;;
+    *)
+        if [[ ${arg} =~ \-\-.* ]]
+        then
+            echo "Invalid flag ${1}. Allowed arguments are (-d|--dryrun; -a|--amend)"
+            exit 1
+        elif [[ ${arg} =~ ${DATE_FORMAT} ]]
+        then
+            date=${arg}
+        else
+            echo "A date should be passed in as the first argument in the form DD-MMM-YYYY e.g. 01-JAN-2023"
+            exit 1
+        fi
+        ;;
     esac
     shift
 done
