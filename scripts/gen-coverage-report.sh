76c57ea76697a2b6959e7c9e5f973ef0903c78aa
Eric Le Fort <ericlefort@live.com>
2023-10-17 20:28:46 -0700

    Added code coverage utility script

---
+++ scripts/gen-coverage-report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                                                                                                                                                                                                
+set -e                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                           
+THIS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )                                                                                                                                                                       
+                                                                                                                                                                                                                                                           
+# Move to the test directory and start the test                                                                                                                                                                                                            
+cd "${THIS_SCRIPT_DIR}/../python/src"                                                                                                                                                                                                                      
+export PYTHONPATH=$(pwd)                                                                                                                                                                                                                                   
+cd ../test                                                                                                                                                                                                                                                 
+coverage run \
+  --source $PYTHONPATH \
+  --branch \
+  -m unittest -v
+coverage report
