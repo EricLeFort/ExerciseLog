1ef4e1a96522e740a83042c64c2a90eed4580b92
Eric Le Fort <ericlefort@live.com>
2023-11-14 20:09:28 -0800

    Extending setup.sh to prepare to use a proper virtualenv setup for the project

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,13 @@
+# Setup the git config for this projct
 git config core.hooksPath .githooks
+
+# Perform some universal python basics
+python3 -m pip install --upgrade pip
+pip3 install virtualenv
+
+# Setup a virtual environment for this project
+VENV_NAME=pyenv
+virtualenv ${VENV_NAME}
+source ${VENV_NAME}/bin/activate
+python3 -m pip install -r requirements.txt
+deactivate
