2a80a25631b06d6aa6b42f1fc732aa1b3f1d4a84
Eric Le Fort <ericlefort@live.com>
2024-07-15 01:57:16 -0700

    Adding TypeScript support

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,3 +12,13 @@ source ${VENV_NAME}/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 deactivate
+
+# Node.js setup
+curl -fsSL https://fnm.vercel.app/install | bash
+fnm use --install-if-missing 20
+
+# Install relevant node packages
+npm install -g npm
+npm install -g typescript
+npm install --save @types/d3
+npm install --save @types/jquery
