5d9b076c40cffa2526dc7bb3e40ee1bb925e55ac
Eric Le Fort <ericlefort@live.com>
2024-10-05 17:22:20 -0700

    Init ios app

---
+++ code/ios/App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Eric Le Fort on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code/ios/App/AppTests/AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by Eric Le Fort on 2024-10-05.
+//
+
+import Testing
+@testable import App
+
+struct AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
